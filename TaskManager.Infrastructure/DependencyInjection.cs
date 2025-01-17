751b2f468d66459cbbd76cdf8c7d8ea07ea99f8a
jalalgoirthm <temitomzi@gmail.com>
2025-01-17 22:22:02 +0100

    Added connection strings to the database

--- TaskManager.Infrastructure/DependencyInjection.cs
+++ TaskManager.Infrastructure/DependencyInjection.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Infrastructure.Data;
 
 namespace TaskManager.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services , IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(""),
+                    migration => migration.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+            });
             return services;
         }
     }
