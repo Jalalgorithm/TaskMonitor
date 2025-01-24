4325e117afc7936c3d436bb242e7bd5134911ab0
jalalgoirthm <temitomzi@gmail.com>
2025-01-24 18:02:44 +0100

    Added mediatr

--- TaskManager.Application/DependencyInjection.cs
+++ TaskManager.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,12 @@ namespace TaskManager.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            
+            services.AddMediatR(mdr =>
+            {
+                mdr.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            }); 
+
+
             return services;
         }
     }
